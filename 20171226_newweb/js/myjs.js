8ef5688b49f6b8ae9e8bd5f1d4330b06b919699b
a08864402 <jacky.chen@timecitytech.com>
2017-12-29 13:54:53 +0800

    rejs

--- 20171226_newweb/js/myjs.js
+++ 20171226_newweb/js/myjs.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     /*topbannerturn*/
     $('.banner_out').slick({ dots: false, infinite: true, slidesToShow: 1, autoplay: true, autoplaySpeed: 6000 });
-    $('.newsrun').slick({ dots: false, infinite: true, slidesToShow: 1, autoplay: true, autoplaySpeed: 0 ,speed:5000});
+    //$('.newsrun').slick({ dots: false, infinite: true, slidesToShow: 1, autoplay: true, autoplaySpeed: 0 ,speed:5000});
     /*niceScroll*/
     //$("html").niceScroll({ cursorwidth: "5px", cursorcolor: "#195d8d", cursorborder: "0px solid #fff", zindex: 999, mousescrollstep: 50, scrollspeed: 50 });
     //$("html").niceScroll({ styler: "fb", cursorcolor: "#6f1602", autohidemode: false, cursorborder: "0px solid #fff", cursorwidth: "8px", zindex: 999 });
@@ -11,28 +11,5 @@ $(document).ready(function() {
     var domminheight = $(window).height() - 35;
     $("body").css("min-height", domminheight);
     /*endallheight*/
-
-    /*animatetop*/
-    $(".top").click(function() {
-        $('html,body').animate({ scrollTop: 0 }, 300);
-    });
-    /*endanimatetop*/
-
-    /*clicklink*/
-    $(".gf-gift").click(function() { window.open(""); });
-    /*endclicklink*/
-
-    /*linkhashanimate*/
-    $(function() {
-
-        $(".awardlistbtn a").click(function() {
-            var target = $(this.hash);
-            $("html,body").animate({
-                scrollTop: target.offset().top
-            }, 500);
-            return false;
-        });
-    });
-    /*endlinkhashanimate*/
 
 });
